4033161b205f1846ae046730219d22c59e60c4b6
Miriam Fernandez <mirindaww@hotmail.com>
2022-10-30 20:49:30 +0100

    Ejercicio 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import {Escena} from './components/escena/Escena';
+import {Story} from './data/Story';
 
 function App() {
   return (
     <div className="App">
-      <Escena />
+      {Story.map((element) => (
+          <Escena key={element.id} text={element.text} />
+        ))}
     </div>
   );
 }
--- src/components/escena/Escena.js
+++ src/components/escena/Escena.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-export const Escena = () => {
+export const Escena = (props) => {
   return (
   <p>
-    "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial"
+    {props.text}
   </p>
   );
 };
