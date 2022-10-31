ff28b0c717f07dcd2016a3b1824e74825f877c45
Miriam Fernandez <mirindaww@hotmail.com>
2022-10-31 01:02:55 +0100

    Ejercicio 5

--- src/App.js
+++ src/App.js
@@ -30,18 +30,41 @@ function App() {
   if(contador == Story[contador].id-1){
     Story[contador].estado = 'activo';
   }
-        
+  
+  //Creo una variable para el mensaje de bienvenida con valor true
+  let [Bienvenida, setBienvenida] = useState(true);
+
+  const Entrar = () =>{
+    setBienvenida(Bienvenida === true? Bienvenida = false : Bienvenida = true);
+  }
   
   return (
     
     <div className="App">
+    {Bienvenida && 
+      <div className='container'>
+        <div className='home'>
+          <h1 className='p-4'>Un heroi molt "Espacial"</h1>
+          <h3 className='mt-5 p-4'>Descubreix l'historia del nostre heroi en un mon tan divertit com ple de riscos</h3>
+          <button type="button" className="btn btn-warning mt-4 p-4" onClick={Entrar}>Veure l'historia</button>
+        </div>
+      </div>
+    }
+    {!Bienvenida &&
+    <div>
       <div>
         <span onClick={Retroceso}><Botones textBtn="Anterior"></Botones></span>
         <span onClick={Avance}><Botones textBtn="Següent"></Botones></span>
       </div>
-      {Story.map((element) => (
+      <div>
+        {Story.map((element) => (
           <Escena key={element.id} text={element.text} estado={element.estado}></Escena>
         ))}
+      </div>
+    </div>  
+    }
+      
+    
     </div>
   );
 }
